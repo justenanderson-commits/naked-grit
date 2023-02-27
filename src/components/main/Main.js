8838d2c551e51dfd6b898e84a87de870dc73bf6d
Justen Anderson <justenanderson@gmail.com>
2023-02-27 15:51:38 -0600

    Add carousel placeholder.

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,6 +1,7 @@
 import './Main.css'
+import Slider from '../slider/Slider'
 import ImageCard from '../imageCard/ImageCard'
-import prints from '../../assets/products'
+import prints from '../../assets/prints'
 
 const Main = () => {
   const displayPrints = prints.map((print) => {
@@ -15,8 +16,17 @@ const Main = () => {
     )
   })
 
+  const containerStyles = {
+    width: "400px",
+    height: "200px",
+    margin: "0 auto"
+  }
+
   return (
     <div className="main">
+      <div style={ containerStyles }>
+      <Slider />
+      </div>
       <h2>Prints</h2>
       <div className="image-cards-container">{displayPrints}</div>
     </div>
