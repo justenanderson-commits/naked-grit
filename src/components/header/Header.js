37e6853d8041e2236e0ac0c6abe3cb221978fe6e
Justen Anderson <justenanderson@gmail.com>
2023-02-27 12:11:24 -0600

    Add header styling.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,23 @@
 import './Header.css'
 import logo from '../../assets/logo.png'
-
+import { Link, NavLink } from 'react-router-dom'
 
 const Header = () => {
-  return ( 
-    <div className='header'>
-      <img src={ logo } width='100px' />
+  return (
+    <div className="header-container">
+      <Link to="/" className="logo-link">
+        <img src={logo} className="logo" />
+      </Link>
+      <ul className="nav-link-container">
+        <li className="nav-link">
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li className="nav-link">
+          <NavLink to="/about">About</NavLink>
+        </li>
+      </ul>
     </div>
-   );
+  )
 }
- 
-export default Header;
+
+export default Header
