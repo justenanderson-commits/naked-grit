416c94a59dec2b15d5fbf37d9bc7cc93703c965b
Justen Anderson <justenanderson@gmail.com>
2023-03-07 09:03:35 -0700

    Refactored styling - moved it to CSS doc.

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,16 +1,9 @@
 import slides from '../../assets/slides'
 import { useState } from 'react'
 import './Slider.css'
-// import Glide, { Controls, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm'
-
-// Left off from this video: https://www.youtube.com/watch?v=SK9AlIbexOE&t=334s at 5:56 //
 
 const Slider = () => {
   const [currentIndex, setCurrentIndex] = useState(5)
-  const sliderStyles = {
-    height: '100%',
-    position: 'relative',
-  }
 
   const slideStyles = {
     width: '100%',
@@ -20,28 +13,6 @@ const Slider = () => {
     backgroundSize: 'cover',
     backgroundImage: `url(${slides[currentIndex].img})`,
     marginBottom: '10%',
-  }
-
-  const leftArrowStyles = {
-    position: 'absolute',
-    top: '50%',
-    transform: 'translate(0, -50%)',
-    left: '3%',
-    fontSize: '45px',
-    color: 'fff',
-    zIndex: 1,
-    cursor: 'pointer',
-  }
-
-  const rightArrowStyles = {
-    position: 'absolute',
-    top: '50%',
-    transform: 'translate(0, -50%)',
-    right: '3%',
-    fontSize: '45px',
-    color: 'fff',
-    zIndex: 1,
-    cursor: 'pointer',
   }
 
   const goToPrevious = () => {
@@ -60,18 +31,26 @@ const Slider = () => {
     setCurrentIndex(newIndex)
   }
 
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex)
+  }
+
   const showDots = slides.map((slide, slideIndex) => (
-    <div className="dotStyles" key={slide.Index}>
+    <div
+      className="dotStyles"
+      key={slide.Index}
+      onClick={() => goToSlide(slideIndex)}
+    >
       ⚪️
     </div>
   ))
 
   return (
-    <div style={sliderStyles}>
-      <div style={leftArrowStyles} onClick={goToPrevious}>
+    <div className="sliderStyles">
+      <div className="arrowStyles leftArrow" onClick={goToPrevious}>
         ◀
       </div>
-      <div style={rightArrowStyles} onClick={goToNext}>
+      <div className="arrowStyles rightArrow" onClick={goToNext}>
         ▶
       </div>
       <div style={slideStyles}></div>
