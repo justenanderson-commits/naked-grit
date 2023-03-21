dd7a913f017e56e64ed813ca4d65b7f8583fd455
Justen Anderson <justenanderson@gmail.com>
2023-03-21 13:54:52 -0600

    Add image to About page.

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,8 +1,12 @@
 import './About.css'
+import paintingHand from '../../assets/painting_hand.jpg'
 
 const About = () => {
   return ( 
-    <div>About Content</div>
+    <div className="about">About Content
+      <img src={ paintingHand } width="400px" alt="Close-up image of a hand painting a flower" />
+    </div>
+    
    );
 }
  
